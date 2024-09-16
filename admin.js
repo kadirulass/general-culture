a791030e7c70e08e978752c8e09bdedb42c3ee08
KADİR ULAŞ <ulaskadir49@gmail.com>
2024-09-16 23:32:40 +0300

    version 1.0.1

--- admin.js
+++ admin.js
@@ -19,45 +19,5 @@ document.getElementById('addQuestionForm').addEventListener('submit', function(e
     document.getElementById('question').value='';
 });
 
-document.getElementById('removeQuestionForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const id = document.getElementById('removeQuestion').value;
-
-    fetch('https://cografya-kelime-oyunu.onrender.com/remove-question', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id })
-    })
-    .then(response => response.text())
-    .then(data => alert(data))
-    .catch(error => console.error('Error:', error));
-});
-
-document.getElementById('removeCategory').addEventListener('change', function() {
-    const category = this.value;
-
-    fetch(`https://cografya-kelime-oyunu.onrender.com/get-questions/${category}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const removeQuestionSelect = document.getElementById('removeQuestion');
-            removeQuestionSelect.innerHTML = ''; // Önceki seçenekleri temizle
-
-            data.forEach(question => {
-                const option = document.createElement('option');
-                option.value = question.id; // ID ile seçim yapmak için
-                option.textContent = question.soru;
-                removeQuestionSelect.appendChild(option);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-});
 
 
